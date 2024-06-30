27fca2615f7dc35c1a278062fe824f49f73ccaa7
Henrico Stoppa <108763444+HenricoStoppa@users.noreply.github.com>
2024-06-30 23:48:52 +0000

    adicionando scripts `lint:check` e  `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 function Home() {
-    return (
-        <div>
-            <h1>Marjorie, te amo!!!!!!</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Marjorie, te amo!!!!!!</h1>
+    </div>
+  );
 }
 
 export default Home;
