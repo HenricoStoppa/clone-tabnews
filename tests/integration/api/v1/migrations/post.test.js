d1ae67375067a031b33969a320b2aff57190761d
Henrico Stoppa <108763444+HenricoStoppa@users.noreply.github.com>
2024-07-22 16:06:59 +0000

    Adicinando endpoint `api/v1/migrations`

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,30 @@
+import database from "infra/database";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const firstResponse = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(firstResponse.status).toBe(201);
+
+  const firstResponseBody = await firstResponse.json();
+  expect(Array.isArray(firstResponseBody)).toBe(true);
+  expect(firstResponseBody.length).toBeGreaterThan(0);
+
+  const SecondResponse = await fetch(
+    "http://localhost:3000/api/v1/migrations",
+    {
+      method: "POST",
+    },
+  );
+  expect(SecondResponse.status).toBe(200);
+
+  const SecondResponseBody = await SecondResponse.json();
+  expect(Array.isArray(SecondResponseBody)).toBe(true);
+  expect(SecondResponseBody.length).toEqual(0);
+});
